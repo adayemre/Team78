f2182d10ec2c61498bb53c8efd091cd0f2065989
Recep Aykurt <recep.aykurt7@gmail.com>
2022-03-11 23:18:58 +0300

    For Demo

--- src/test/java/Hooks/Hooks.java
+++ src/test/java/Hooks/Hooks.java
@@ -3,25 +3,57 @@ package Hooks;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import utilities.ConfigurationReader;
 import utilities.Driver;
 
 public class Hooks {
+
+    public static RequestSpecification spec;
+
     @Before
     public void setUp(){
 
 
     }
 
+    @Before(order = 1, value = "@UIRegistration")
+    public void navigateToRegistrationPage(){
+
+        Driver.getDriver().get(ConfigurationReader.getProperty("medunna_registration_url"));
+
+    }
+
+    @Before(order = 1, value = "@Appointment")
+    public void navigateToAppointmentPage(){
+
+        Driver.getDriver().get(ConfigurationReader.getProperty("medunna_appointment_url"));
+
+    }
+
+    @Before(value = "@ApiRegistrant")
+    public void setUpApi(){
+        spec = new RequestSpecBuilder().setBaseUri("https://medunna.com").build();
+    }
+
+    @Before(value = "@ApiRegistrant2")
+    public void setUpApi2(){
+        spec = new RequestSpecBuilder().setBaseUri("https://medunna.com").build();
+    }
+
     @After
     public void tearDown(Scenario scenario){
 
-        if (scenario.isFailed()) {
-            final byte[] screenshot=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+//        if (scenario.isFailed()) {
+//            final byte[] screenshot=((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+//
+//            scenario.attach(screenshot, "image/png","screenshots");
+//        }
 
-            scenario.attach(screenshot, "image/png","screenshots");
-        }
+        Driver.closeDriver();
 
     }
 
